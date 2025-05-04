d9fdcb72135f1b06f5cc88df2e79dc68753643c0
somerandomdudelmfao <pew5670@gmail.com>
2025-05-04 15:51:24 +0100

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -117,15 +117,15 @@ getgenv().ExunysDeveloperAimbot = {
 		Enabled = true,
 		Visible = true,
 
-		Radius = 180,
+		Radius = 360,
 		NumSides = 60,
 
 		Thickness = 1,
 		Transparency = 1,
 		Filled = false,
 
-		RainbowColor = false,
-		RainbowOutlineColor = false,
+		RainbowColor = true,
+		RainbowOutlineColor = true,
 		Color = Color3fromRGB(255, 255, 255),
 		OutlineColor = Color3fromRGB(0, 0, 0),
 		LockedColor = Color3fromRGB(255, 150, 150)
